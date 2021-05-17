40f1075a513a17886edd627cd09a46b4c5a60a4a
Jay <jayseetow@gmail.com>
2021-05-17 22:27:12 +0100

    First use of TcpListener in main.rs

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,11 @@
+use std::net::TcpListener;
+
 fn main() {
-    println!("Hello, world!");
+    let listener = TcpListener::bind("127.0.0.1:7878").unwrap(); // should normally handle the errors
+
+    for stream in listener.incoming() {
+        let _stream = stream.unwrap();
+
+        println!("Connection established!");
+    }
 }
