72d2d723f8716d74021acafe569639fb05e6026e
jay <jayseetow@gmail.com>
2021-05-20 16:03:17 +0100

    Added 404 handling to main.rs.

--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,5 @@
-use std::{fs, io::prelude::*};
 use std::net::{TcpListener, TcpStream};
+use std::{fs, io::prelude::*};
 
 fn main() {
     let listener = TcpListener::bind("127.0.0.1:7878").unwrap(); // should normally handle the errors
@@ -12,20 +12,53 @@ fn main() {
     }
 }
 
-fn handle_connection(mut stream: TcpStream) { // stream needs to be mutable because the stream's internal state might change (e.g. receiving more data)
+fn handle_connection(mut stream: TcpStream) {
+    // stream needs to be mutable because the stream's internal state might change (e.g. receiving more data)
     let mut buffer = [0; 1024]; // buffer management needs to be more complicated to handle requests of arbitrary size
 
     stream.read(&mut buffer).unwrap();
 
     println!("Request: {}", String::from_utf8_lossy(&buffer[..]));
 
-    let contents = fs::read_to_string("hello.html").unwrap();
+    let get = b"GET / HTTP/1.1\r\n";
 
-    let response = format!(
-        "HTTP/1.1 200 OK\r\nContent-Length: {}\r\n\r\n{}", // Content-Length header added to make the response valid HTTP.
-        contents.len(),
-        contents
-    );
-    stream.write(response.as_bytes()).unwrap();
-    stream.flush().unwrap();
+    if buffer.starts_with(get) {
+        let contents = fs::read_to_string("hello.html").unwrap();
+
+        let response = format!(
+            "HTTP/1.1 200 OK\r\nContent-Length: {}\r\n\r\n{}", // Content-Length header added to make the response valid HTTP.
+            contents.len(),
+            contents
+        );
+        stream.write(response.as_bytes()).unwrap();
+        stream.flush().unwrap();
+    } else {
+        let status_line = "HTTP/1.1 404 NOT FOUND";
+        let contents = fs::read_to_string("404.html").unwrap();
+        let response = format!(
+            "{}\r\nContent-Length: {}\r\n\r\n{}", // Content-Length header added to make the response valid HTTP.
+            contents.len(),
+            contents
+        );
+        stream.write(response.as_bytes()).unwrap();
+        stream.flush().unwrap();
+    }
 }
+
+// fn handle_connection(mut stream: TcpStream) { // stream needs to be mutable because the stream's internal state might change (e.g. receiving more data)
+//     let mut buffer = [0; 1024]; // buffer management needs to be more complicated to handle requests of arbitrary size
+
+//     stream.read(&mut buffer).unwrap();
+
+//     println!("Request: {}", String::from_utf8_lossy(&buffer[..]));
+
+//     let contents = fs::read_to_string("hello.html").unwrap();
+
+//     let response = format!(
+//         "HTTP/1.1 200 OK\r\nContent-Length: {}\r\n\r\n{}", // Content-Length header added to make the response valid HTTP.
+//         contents.len(),
+//         contents
+//     );
+//     stream.write(response.as_bytes()).unwrap();
+//     stream.flush().unwrap();
+// }
