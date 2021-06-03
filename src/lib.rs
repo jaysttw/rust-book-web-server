b2c490532f96a847af1e93e8cf7a634d4d8f8849
jay <jayseetow@gmail.com>
2021-06-03 15:34:12 +0100

    Fixed receiver to let it be passed amongst multiple Workers.

--- src/lib.rs
+++ src/lib.rs
@@ -1,14 +1,11 @@
-use std::{thread::Thread, usize};
-use std::sync::mpsc;
+use std::{sync::{Mutex, mpsc, Arc}, thread::Thread, usize};
 
 pub struct ThreadPool {
     workers: Vec<Worker>,
     sender: mpsc::Sender<Job>,
 }
 
-struct Job {
-
-}
+type Job = Box<dyn FnOnce() + Send + 'static>;
 
 impl ThreadPool {
     // pub fn new(size: usize) -> Result<ThreadPool, PoolCreationError> {
@@ -23,11 +20,12 @@ impl ThreadPool {
         assert!(size > 0);
 
         let (sender, receiver) = mpsc::channel();
+        let receiver = Arc::new(Mutex::new(receiver));
         
         let mut workers = Vec::with_capacity(size); // `with_capacity` pre-allocates fixed size
 
         for id in 0..size {
-            workers.push(Worker::new(id))
+            workers.push(Worker::new(id, Arc::clone(&receiver)))
         }
 
         ThreadPool { workers, sender }
@@ -42,12 +40,14 @@ impl ThreadPool {
 
 struct Worker {
     id: usize,
-    thread: thread::JoinHandle<()>,
+    thread: std::thread::JoinHandle<()>,
 }
 
 impl Worker {
-    fn new(id: usize) -> Worker {
-        let thread = thread::spawn(|| {});
+    fn new(id: usize, receiver: Arc<Mutex<mpsc::Receiver<Job>>>) -> Worker {
+        let thread = std::thread::spawn(|| {
+            receiver;
+        });
 
         Worker { id, thread }
     }
