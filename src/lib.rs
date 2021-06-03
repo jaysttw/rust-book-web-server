844ec44d381fd33dc7d95ee6bd584b429a15e7c9
Jay <jayseetow@gmail.com>
2021-06-03 14:57:47 +0100

    Added Worker struct.

--- src/lib.rs
+++ src/lib.rs
@@ -1,7 +1,7 @@
 use std::{thread::Thread, usize};
 
 pub struct ThreadPool {
-    threads: Vec<thread::JoinHandle<()>>
+    workers: Vec<Worker>
 };
 
 impl ThreadPool {
@@ -16,18 +16,31 @@ impl ThreadPool {
         /// The `new` function will panic if the size is zero.
         assert!(size > 0);
         
-        let mut threads = Vec::with_capacity(size); // `with_capacity` pre-allocates fixed size
+        let mut workers = Vec::with_capacity(size); // `with_capacity` pre-allocates fixed size
 
-        for _ in 0..size {
-            // do something
+        for id in 0..size {
+            workers.push(Worker::new(id))
         }
 
-        ThreadPool { threads }
+        ThreadPool { workers }
     }
 
     pub fn execute<F>(&self, f:F)
     where F: FnOnce() + Send + 'static,
     {
         // Should have a similar interface to `thread::spawn`.
+    }
+}
+
+struct Worker {
+    id: usize,
+    thread: thread::JoinHandle<()>,
+}
+
+impl Worker {
+    fn new(id: usize) -> Worker {
+        let thread = thread::spawn(|| {});
+
+        Worker { id, thread }
     }
 }
