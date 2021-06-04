7842b90e7d3f94d498933a100b6e3e753b2c58fa
Jay <jayseetow@gmail.com>
2021-06-04 16:29:01 +0100

    Added sending of `Terminate` messages for ThreadPool `drop`.

--- src/lib.rs
+++ src/lib.rs
@@ -43,6 +43,14 @@ impl ThreadPool {
 
 impl Drop for ThreadPool {
     fn drop(&mut self) {
+        println!("Sending Terminate message to all workers.");
+
+        for _ in self.workers {
+            self.sender.send(Message::Terminate).unwrap();
+        }
+
+        println!("Shutting down all workers.");
+        
         for worker in &mut self.workers {
             println!("Shutting down worker {}", worker.id);
 
